85402be5962e92912acf7bddca731fe17bdbd580
Yash Vats <66666377+vatsyashh@users.noreply.github.com>
2021-10-16 16:52:58 +0530

    Add files via upload

---
+++ jspsych/plugins/jspsych-survey-audio-multi-choice.js
@@ -0,0 +1,384 @@
+
+
+/**
+ * jspsych-survey-multi-choice
+ * a jspsych plugin for multiple choice survey questions
+ *
+ * Shane Martin
+ *
+ * documentation: docs.jspsych.org
+ *
+ */
+
+
+jsPsych.plugins['survey-audio-multi-choice'] = (function() {
+  var plugin = {};
+
+  plugin.info = {
+    name: 'survey-audio-multi-choice',
+    description: '',
+    parameters: {
+      stimulus: {
+        type: jsPsych.plugins.parameterType.AUDIO,
+        pretty_name: 'Stimulus',
+        default: undefined,
+        description: 'The audio to be played.'
+      },
+      questions: {
+        type: jsPsych.plugins.parameterType.COMPLEX,
+        array: true,
+        pretty_name: 'Questions',
+        nested: {
+          prompt: {
+            type: jsPsych.plugins.parameterType.STRING,
+            pretty_name: 'Prompt',
+            default: undefined,
+            description: 'The strings that will be associated with a group of options.'
+          },
+          options: {
+            type: jsPsych.plugins.parameterType.STRING,
+            pretty_name: 'Options',
+            array: true,
+            default: undefined,
+            description: 'Displays options for an individual question.'
+          },
+          required: {
+            type: jsPsych.plugins.parameterType.BOOL,
+            pretty_name: 'Required',
+            default: false,
+            description: 'Subject will be required to pick an option for each question.'
+          },
+          horizontal: {
+            type: jsPsych.plugins.parameterType.BOOL,
+            pretty_name: 'Horizontal',
+            default: false,
+            description: 'If true, then questions are centered and options are displayed horizontally.'
+          },
+          name: {
+            type: jsPsych.plugins.parameterType.STRING,
+            pretty_name: 'Question Name',
+            default: '',
+            description: 'Controls the name of data values associated with this question'
+          }
+        }
+      },
+      trial_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'The maximum duration to wait for a response.'
+      },
+      randomize_question_order: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Randomize Question Order',
+        default: false,
+        description: 'If true, the order of the questions will be randomized'
+      },
+      preamble: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Preamble',
+        default: null,
+        description: 'HTML formatted string to display at the top of the page above all the questions.'
+      },
+      button_label: {
+        type: jsPsych.plugins.parameterType.STRING,
+        pretty_name: 'Button label',
+        default:  'Continue',
+        description: 'Label of the button.'
+      },
+      autocomplete: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Allow autocomplete',
+        default: false,
+        description: "Setting this to true will enable browser auto-complete or auto-fill for the form."
+      },
+      trial_ends_after_audio: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Trial ends after audio',
+        default: false,
+        description: 'If true, then the trial will end as soon as the audio file finishes playing.'
+      },
+      response_allowed_while_playing: {
+        type: jsPsych.plugins.parameterType.BOOL,
+        pretty_name: 'Response allowed while playing',
+        default: true,
+        description: 'If true, then responses are allowed while the audio is playing. ' +
+          'If false, then the audio must finish playing before a response is accepted.'
+      }
+    }
+  }
+  plugin.trial = function(display_element, trial) {
+    var plugin_id_name = "jspsych-survey-multi-choice";
+
+    var html = "";
+
+    var context = jsPsych.pluginAPI.audioContext();
+    var audio;
+
+    // store response
+    var response = {
+      rt: null,
+      button: null
+    };
+    var startTime;
+
+
+
+    jsPsych.pluginAPI.getAudioBuffer(trial.stimulus)
+      .then(function (buffer) {
+        if (context !== null) {
+          audio = context.createBufferSource();
+          audio.buffer = buffer;
+          audio.connect(context.destination);
+        } else {
+          audio = buffer;
+          audio.currentTime = 0;
+        }
+        setupTrial();
+      })
+      .catch(function (err) {
+        console.error(`Failed to load audio file "${trial.stimulus}". Try checking the file path. We recommend using the preload plugin to load audio files.`)
+        console.error(err)
+      });
+
+    function setupTrial() {
+      // set up end event if trial needs it
+      if (trial.trial_ends_after_audio) {
+        audio.addEventListener('ended', end_trial);
+      }
+
+      // enable buttons after audio ends if necessary
+      if ((!trial.response_allowed_while_playing) & (!trial.trial_ends_after_audio)) {
+        audio.addEventListener('ended', enable_buttons);
+      }
+
+      //display buttons
+      // var buttons = [];
+      // if (Array.isArray(trial.button_html)) {
+      //   if (trial.button_html.length == trial.choices.length) {
+      //     buttons = trial.button_html;
+      //   } else {
+      //     console.error('Error in audio-button-response plugin. The length of the button_html array does not equal the length of the choices array');
+      //   }
+      // } else {
+      //   for (var i = 0; i < trial.choices.length; i++) {
+      //     buttons.push(trial.button_html);
+      //   }
+      // }
+
+      // if (trial.response_allowed_while_playing) {
+      //   enable_buttons();
+      // } else {
+      //   disable_buttons();
+      // }
+
+
+
+       startTime = performance.now();
+
+      // start audio
+      if (context !== null) {
+        startTime = context.currentTime;
+        audio.start(startTime);
+      } else {
+        audio.play();
+      }
+
+      // end trial if time limit is set
+ 
+    }
+
+
+
+
+    // function to end trial when it is time
+    function end_trial() {
+
+      // kill any remaining setTimeout handlers
+      jsPsych.pluginAPI.clearAllTimeouts();
+
+      // stop the audio file if it is playing
+      // remove end event listeners if they exist
+      if (context !== null) {
+        audio.stop();
+      } else {
+        audio.pause();
+      }
+
+      // audio.removeEventListener('ended', end_trial);
+      // audio.removeEventListener('ended', enable_buttons);
+
+      // gather the data to store for the trial
+      var trial_data = {
+        rt: response.rt,
+        stimulus: trial.stimulus,
+        response: response.button
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+    }
+
+    function button_response(e) {
+      var choice = e.currentTarget.getAttribute('data-choice'); // don't use dataset for jsdom compatibility
+      after_response(choice);
+    }
+
+    function disable_buttons() {
+      var btns = document.querySelectorAll('.jspsych-audio-button-response-button');
+      for (var i = 0; i < btns.length; i++) {
+        var btn_el = btns[i].querySelector('button');
+        if (btn_el) {
+          btn_el.disabled = true;
+        }
+        btns[i].removeEventListener('click', button_response);
+      }
+    }
+
+    function enable_buttons() {
+      var btns = document.querySelectorAll('.jspsych-audio-button-response-button');
+      for (var i = 0; i < btns.length; i++) {
+        var btn_el = btns[i].querySelector('button');
+        if (btn_el) {
+          btn_el.disabled = false;
+        }
+        btns[i].addEventListener('click', button_response);
+      }
+    }
+
+
+
+
+
+
+
+
+    // inject CSS for trial
+    html += '<style id="jspsych-survey-multi-choice-css">';
+    html += ".jspsych-survey-multi-choice-question { margin-top: 2em; margin-bottom: 2em; text-align: left; }"+
+      ".jspsych-survey-multi-choice-text span.required {color: darkred;}"+
+      ".jspsych-survey-multi-choice-horizontal .jspsych-survey-multi-choice-text {  text-align: center;}"+
+      ".jspsych-survey-multi-choice-option { line-height: 2; }"+
+      ".jspsych-survey-multi-choice-horizontal .jspsych-survey-multi-choice-option {  display: inline-block;  margin-left: 1em;  margin-right: 1em;  vertical-align: top;}"+
+      "label.jspsych-survey-multi-choice-text input[type='radio'] {margin-right: 1em;}";
+    html += '</style>';
+
+    // show preamble text
+    if(trial.preamble !== null){
+      html += '<div id="jspsych-survey-likert-preamble" class="jspsych-survey-likert-preamble" style="border: thin solid #0000FF ; border-radius: 10px ; background-color: #e7ecfd;">' +
+        trial.preamble +
+        "</div>";
+    }
+
+    // form element
+    if ( trial.autocomplete ) {
+      html += '<form id="jspsych-survey-multi-choice-form">';
+    } else {
+      html += '<form id="jspsych-survey-multi-choice-form" autocomplete="off">';
+    }
+    // generate question order. this is randomized here as opposed to randomizing the order of trial.questions
+    // so that the data are always associated with the same question regardless of order
+    var question_order = [];
+    for(var i=0; i<trial.questions.length; i++){
+      question_order.push(i);
+    }
+    if(trial.randomize_question_order){
+      question_order = jsPsych.randomization.shuffle(question_order);
+    }
+    
+    // add multiple-choice questions
+    for (var i = 0; i < trial.questions.length; i++) {
+      
+      // get question based on question_order
+      var question = trial.questions[question_order[i]];
+      var question_id = question_order[i];
+      
+      // create question container
+      var question_classes = ['jspsych-survey-multi-choice-question'];
+      if (question.horizontal) {
+        question_classes.push('jspsych-survey-multi-choice-horizontal');
+      }
+
+      html += '<div id="jspsych-survey-multi-choice-'+question_id+'" class="'+question_classes.join(' ')+'"  data-name="'+question.name+'">';
+
+      // add question text
+      html += '<p class="jspsych-survey-multi-choice-text survey-multi-choice">' + question.prompt 
+      if(question.required){
+        html += "<span class='required'>*</span>";
+      }
+      html += '</p>';
+
+      // create option radio buttons
+      for (var j = 0; j < question.options.length; j++) {
+        // add label and question text
+        var option_id_name = "jspsych-survey-multi-choice-option-"+question_id+"-"+j;
+        var input_name = 'jspsych-survey-multi-choice-response-'+question_id;
+        var input_id = 'jspsych-survey-multi-choice-response-'+question_id+'-'+j;
+
+        var required_attr = question.required ? 'required' : '';
+
+        // add radio button container
+        html += '<div id="'+option_id_name+'" class="jspsych-survey-multi-choice-option">';
+        html += '<label class="jspsych-survey-multi-choice-text" for="'+input_id+'">';
+        html += '<input type="radio" name="'+input_name+'" id="'+input_id+'" value="'+question.options[j]+'" '+required_attr+'></input>';
+        html += question.options[j]+'</label>';
+        html += '</div>';
+      }
+
+      html += '</div>';
+    }
+    
+    // add submit button
+    html += '<input type="submit" id="'+plugin_id_name+'-next" class="'+plugin_id_name+' jspsych-btn"' + (trial.button_label ? ' value="'+trial.button_label + '"': '') + '></input>';
+    html += '</form>';
+
+    // render
+    display_element.innerHTML = html;
+
+    document.querySelector('form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // measure response time
+      var endTime = performance.now();
+      var response_time = endTime - startTime;
+
+      // create object to hold responses
+      var question_data = {};
+      for(var i=0; i<trial.questions.length; i++){
+        var match = display_element.querySelector('#jspsych-survey-multi-choice-'+i);
+        var id = "Q" + i;
+        if(match.querySelector("input[type=radio]:checked") !== null){
+          var val = match.querySelector("input[type=radio]:checked").value;
+        } else {
+          var val = "";
+        }
+        var obje = {};
+        var name = id;
+        if(match.attributes['data-name'].value !== ''){
+          name = match.attributes['data-name'].value;
+        }
+        obje[name] = val;
+        Object.assign(question_data, obje);
+      }
+      // save data
+      var trial_data = {
+        rt: response_time,
+        response: question_data,
+        question_order: question_order
+      };
+      display_element.innerHTML = '';
+
+      // next trial
+      audio.pause();
+      // audio.stop();
+      jsPsych.finishTrial(trial_data);
+
+    });
+
+    var startTime = performance.now();
+  };
+
+  return plugin;
+})();
